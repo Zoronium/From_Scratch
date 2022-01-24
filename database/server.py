0a8cb6df655f65d2fc04d60ca177822e81fe8592
Advait Jain <89680252+Zoronium@users.noreply.github.com>
2022-01-24 22:09:35 +0530

    update to multi threading
    multi threading to be added just as additional features

--- database/server.py
+++ database/server.py
@@ -1,39 +1,89 @@
-# first of all import the socket library
+# import socket programming library
 import socket
 
-# next create a socket object
-s = socket.socket()
-print("Socket successfully created")
+# import thread module
+from _thread import *
+import threading
 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345
+print_lock = threading.Lock()
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))
-print("socket binded to %s" % (port))
 
-# put the socket into listening mode
-s.listen(5)
-print("socket is listening")
+temp_file = {}
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
+perf_act = {
+    'get': 'GET',
+    'set': 'SET',
+    'mget': 'MGET',
+    'mset': 'MSET',
+    'flush': 'FLUSH',
+    'delete': "DELETE"
+}
+prfix = { # to add the working here 
+    '$': handler_binary,
+    '-': handler_Error,
+    ':': handler_int,
+    '*': handler_array,
+    '%': handler_dict,
+    '+': handler_string,
+}
 
-    # Establish connection with client.
-    c, addr = s.accept()
-    print('Got connection from', addr)
+# thread function
+def checking(file_recv):
+	if file_recv[:2] :
+		pass
 
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
+def handler_protocol(file_r):
+	pass
 
-    # Close the connection with the client
+def threaded(c):
+    while True:
+
+        # data received from client
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+
+            # lock released on exit
+            print_lock.release()
+            break
+            # reverse the given string from client
+
+        # send back reversed string to client
+        c.send(data)
+
+    # connection closed
     c.close()
 
-    # Breaking once connection closed
-    break
+
+def Main():
+    host = ""
+
+    # reverse a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to port", port)
+
+    # put the socket into listening mode
+    s.listen(5)
+    print("socket is listening")
+
+    # a forever loop until client wants to exit
+    while True:
+
+        # establish connection with client
+        c, addr = s.accept()
+
+        # lock acquired by client
+        print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
+
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,))
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
