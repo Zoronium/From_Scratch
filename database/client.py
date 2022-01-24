4b1b7c0fe815067d4bd87862c96ab12fc940ec06
Advait Jain <89680252+Zoronium@users.noreply.github.com>
2022-01-24 22:07:37 +0530

    update client
    just added some improvements

--- database/client.py
+++ database/client.py
@@ -1,16 +1,32 @@
 # Import socket module
+
 import socket			
+def Main():
+    # Create a socket object
+    s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)		
 
-# Create a socket object
-s = socket.socket()		
+    # Define the port on which you want to connect
+    port = 12345			
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', port))
 
-# Define the port on which you want to connect
-port = 12345			
+    message = "this is not a secure messaage"
+    while True:
+        s.send(message.encode())
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
+        data = s.recv(1024)
 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close()	
+        # receive data from the server and decoding to get the string.
+        print ('Recived from server : ' , str(data.decode()))
+
+        ans = input("Do you want to continue  :")
+        if ans.lower() =="y":
+            continue
+        else:
+            break
+
+        # close the connection
+    s.close()	
+
+if __name__ == '__main__':
+    Main()
